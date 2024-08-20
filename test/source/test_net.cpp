2999e31a207fae59a9806a8b351737d7bdbb76ec
Dilshod Mukhtarov <dilshodm@gmail.com>
2024-08-20 18:11:09 +0400

    Disable clazy warning in test file

--- test/source/test_net.cpp
+++ test/source/test_net.cpp
@@ -8,6 +8,8 @@
 #include <scorbit_sdk/scorbit_sdk.h>
 #include <catch2/catch_test_macros.hpp>
 
+// clazy:excludeall=non-pod-global-static
+
 TEST_CASE("Version") {
     static_assert(std::string_view(SCORBIT_SDK_VERSION) == std::string_view("0.1.0"));
     CHECK(std::string(SCORBIT_SDK_VERSION) == std::string("0.1.0"));
