0c2ba15044b9113c5b33c447ac6798742d24f0ab
Dilshod Mukhtarov <dilshodm@gmail.com>
2024-08-23 17:21:35 +0400

    Documentation in log.h and log_types.h

--- include/scorbit_sdk/log.h
+++ include/scorbit_sdk/log.h
@@ -11,7 +11,38 @@
 
 namespace scorbit {
 
+/**
+ * @brief Registers a logger callback function to be invoked for log messages.
+ *
+ * This function allows the user to register a custom logger callback that will be called
+ * whenever a log message is generated. The callback provides detailed information about
+ * the log event, including the message, log level, source file, line number, and any
+ * user-provided data.
+ *
+ * @param loggerFunction The logger callback function to be registered. It should have the
+ *                       signature specified by @ref scorbit::logger_callback_t "".
+ * @param userData A pointer to user-defined data that will be passed to the logger callback
+ *                 each time it is invoked. This parameter is optional and defaults to `nullptr`.
+ *
+ * @note The logger function does not need to be thread-safe, as the logging mechanism ensures
+ *       thread safety internally.
+ *
+ * @warning The logger function can be registered multiple times, but only the most recently
+ *          registered callback will be active. Previous registrations will be overwritten.
+ *
+ * @see unregisterLogger()
+ */
 extern void registerLogger(logger_callback_t &&loggerFunction, void *userData = nullptr);
+
+/**
+ * @brief Unregisters the currently registered logger callback function.
+ *
+ * This function removes the logger callback function that was previously registered
+ * using @ref registerLogger(). After this call, no logger callback will be invoked
+ * until a new one is registered.
+ *
+ * @see registerLogger()
+ */
 extern void unregisterLogger();
 
 } // namespace scorbit
--- include/scorbit_sdk/log_types.h
+++ include/scorbit_sdk/log_types.h
@@ -12,23 +12,41 @@
 
 namespace scorbit {
 
-enum class LogLevel
-{
+/**
+ * @enum LogLevel
+ * @brief Defines the log levels for the logger.
+ */
+
+enum class LogLevel {
+    /** @brief Debug log level */
     Debug,
+
+    /** @brief Info log level */
     Info,
+
+    /** @brief Warning log level */
     Warn,
+
+    /** @brief Error log level */
     Error,
 };
 
 /**
- * @brief Logger function type
- * @param msg Log message
- * @param level log level (LogLevel::Debug, LogLevel::Info, LogLevel::Warn, LogLevel::Error)
- * @param file file name
- * @param line line number
- * @param user pointer to arbitrary user data
+ * @brief Type definition for the logger callback function.
+ *
+ * This function type is used for logging messages, allowing clients to register a custom
+ * logging function that will be called with detailed information about each log event.
+ *
+ * The callback function takes the following parameters:
+ * - **msg**: The log message content.
+ * - **level**: The log level, indicating the severity of the message. This is of type
+ *              @ref scorbit::LogLevel.
+ * - **file**: The name of the source file where the log message was generated.
+ * - **line**: The line number in the source file where the log message was generated.
+ * - **userData**: A pointer to arbitrary user data, provided by the client when registering
+ *                 the callback. This can be used to pass context or state information.
  */
 using logger_callback_t = std::function<void(std::string_view msg, LogLevel level, const char *file,
-                                             int line, void *user)>;
+                                             int line, void *userData)>;
 
 } // namespace scorbit
