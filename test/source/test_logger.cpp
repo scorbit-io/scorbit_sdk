8990ef596fea020ed2eea7d774d554644f91e9e8
Dilshod Mukhtarov <dilshodm@gmail.com>
2024-08-23 15:33:10 +0400

    Add lambda and bind cases to test_logger

--- test/source/test_logger.cpp
+++ test/source/test_logger.cpp
@@ -90,21 +90,66 @@ TEST_CASE("logger using function callback")
 {
     static CallbackHelper userData;
 
-    SECTION("registerLogger")
-    {
-        scorbit::registerLogger(callback, &userData);
-        INF("Hello");
-        int line = __LINE__ - 1;
-        const char *file = __FILE__;
-        CHECK(userData.msg == "Hello");
-        CHECK(userData.level == scorbit::LogLevel::Info);
-        CHECK(userData.file == file + std::string_view(file).find_last_of("/\\") + 1);
-        CHECK(userData.line == line);
+    scorbit::registerLogger(callback, &userData);
+    INF("Hello");
+    int line = __LINE__ - 1;
+    const char *file = __FILE__;
+    CHECK(userData.msg == "Hello");
+    CHECK(userData.level == scorbit::LogLevel::Info);
+    CHECK(userData.file == file + std::string_view(file).find_last_of("/\\") + 1);
+    CHECK(userData.line == line);
 
-        // After unregister log it does no longer log (in the function object it's still old values)
-        scorbit::unregisterLogger();
-        INF("new logs");
-        CHECK(userData.msg == "Hello");
-        CHECK(userData.line == line);
-    }
+    // After unregister log it does no longer log (in the function object it's still old values)
+    scorbit::unregisterLogger();
+    INF("new logs");
+    CHECK(userData.msg == "Hello");
+    CHECK(userData.line == line);
+}
+
+TEST_CASE("logger using lambda callback")
+{
+    CallbackHelper userData;
+
+    scorbit::registerLogger([&userData](std::string_view msg, scorbit::LogLevel level,
+                                        const char *file, int line, void *user) {
+        (void)user;
+        userData.level = level;
+        userData.file = file;
+        userData.line = line;
+        userData.msg = msg;
+    });
+    INF("Hello");
+    int line = __LINE__ - 1;
+    const char *file = __FILE__;
+    CHECK(userData.msg == "Hello");
+    CHECK(userData.level == scorbit::LogLevel::Info);
+    CHECK(userData.file == file + std::string_view(file).find_last_of("/\\") + 1);
+    CHECK(userData.line == line);
+
+    // After unregister log it does no longer log (in the function object it's still old values)
+    scorbit::unregisterLogger();
+    INF("new logs");
+    CHECK(userData.msg == "Hello");
+    CHECK(userData.line == line);
+}
+
+TEST_CASE("logger using bind callback")
+{
+    CallbackHelper userData;
+
+    scorbit::registerLogger(std::bind(callback, std::placeholders::_1, std::placeholders::_2,
+                                      std::placeholders::_3, std::placeholders::_4, &userData));
+    INF("Hello");
+    int line = __LINE__ - 1;
+    const char *file = __FILE__;
+    CHECK(userData.msg == "Hello");
+    CHECK(userData.level == scorbit::LogLevel::Info);
+    CHECK(userData.file == file + std::string_view(file).find_last_of("/\\") + 1);
+    CHECK(userData.line == line);
+
+    // After unregister log it does no longer log (in the function object it's still old values)
+    scorbit::unregisterLogger();
+    INF("new logs");
+    CHECK(userData.msg == "Hello");
+    CHECK(userData.line == line);
 }
