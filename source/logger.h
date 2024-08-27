582d8516668038b6499f97efc6a679d883059e6c
Dilshod Mukhtarov <dilshodm@gmail.com>
2024-08-27 09:18:43 +0400

    Fix: build on MacOS
    -Wgnu-zero-variadic-macro-arguments

--- source/logger.h
+++ source/logger.h
@@ -36,17 +36,16 @@ private:
 extern Logger *logger();
 
 // Macros to make the logMessage function easier to call with added filename and line number
-#define DBG(message, ...) logMessage(message, LogLevel::Debug, __FILE__, __LINE__, ##__VA_ARGS__)
-#define INF(message, ...) logMessage(message, LogLevel::Info, __FILE__, __LINE__, ##__VA_ARGS__)
-#define WRN(message, ...) logMessage(message, LogLevel::Warn, __FILE__, __LINE__, ##__VA_ARGS__)
-#define ERR(message, ...) logMessage(message, LogLevel::Error, __FILE__, __LINE__, ##__VA_ARGS__)
+#define DBG(...) logMessage(LogLevel::Debug, __FILE__, __LINE__, __VA_ARGS__)
+#define INF(...) logMessage(LogLevel::Info, __FILE__, __LINE__, __VA_ARGS__)
+#define WRN(...) logMessage(LogLevel::Warn, __FILE__, __LINE__, __VA_ARGS__)
+#define ERR(...) logMessage(LogLevel::Error, __FILE__, __LINE__, __VA_ARGS__)
 
 template<typename... Args>
-constexpr inline void logMessage(std::string_view message, LogLevel level, const char *file,
-                                 int line, Args &&...args)
+constexpr inline void logMessage(LogLevel level, const char *file, int line, Args &&...args)
 {
     // Instead of full file path, we just want the file name
-    logger()->log(fmt::format(message, std::forward<Args>(args)...), level,
+    logger()->log(fmt::format(std::forward<Args>(args)...), level,
                   file + std::string_view(file).find_last_of("/\\") + 1, line);
 }
 
