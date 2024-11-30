0ac6093ee23faef24555ed499c20130dc5b32330
Dilshod Mukhtarov <dilshodm@gmail.com>
2024-11-30 18:39:07 +0400

    Fix: Boost

---
+++ cmake/boost.cmake
@@ -0,0 +1,34 @@
+set(BOOS_INCLUDE_LIBRARIES "")
+list(APPEND BOOST_INCLUDE_LIBRARIES "${BOOST_NOT_HEADER_ONLY_COMPONENTS_THAT_YOU_NEED}")
+list(APPEND BOOST_INCLUDE_LIBRARIES "${BOOST_HEADER_ONLY_COMPONENTS_THAT_YOU_NEED}")
+
+# url for 1.85.0 + only
+set(BOOST_URL
+    "https://github.com/boostorg/boost/releases/download/boost-${TRY_BOOST_VERSION}/boost-${TRY_BOOST_VERSION}-cmake.tar.xz"
+)
+
+set(Boost_USE_STATIC_LIBS ON) # only find static libs
+set(Boost_USE_DEBUG_LIBS OFF) # ignore debug libs and
+set(Boost_USE_RELEASE_LIBS ON) # only find release libs
+set(Boost_USE_MULTITHREADED ON)
+set(Boost_USE_STATIC_RUNTIME OFF)
+
+cmake_policy(SET CMP0167 NEW)
+CPMAddPackage(
+    NAME Boost
+    URL ${BOOST_URL}
+    EXCLUDE_FROM_ALL YES
+    OPTIONS "BOOST_ENABLE_CMAKE ON" "BOOST_SKIP_INSTALL_RULES OFF"
+)
+
+set(BOOST_LIBS "") # Initialize an empty list to collect libraries
+
+# Collect header-only Boost libraries
+foreach(a_lib ${BOOST_HEADER_ONLY_COMPONENTS_THAT_YOU_NEED})
+    list(APPEND BOOST_LIBS boost_${a_lib})
+endforeach()
+
+# Collect non-header-only Boost libraries
+foreach(a_lib ${BOOST_NOT_HEADER_ONLY_COMPONENTS_THAT_YOU_NEED})
+    list(APPEND BOOST_LIBS Boost::${a_lib})
+endforeach()
