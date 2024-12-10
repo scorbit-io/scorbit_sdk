dd051177b9d0d73d54072ff62bb5e247455312a0
Dilshod Mukhtarov <dilshodm@gmail.com>
2024-12-10 16:36:32 +0400

    Add support for buildroot docker and build script
    Using toolchain provided by Pedretti

--- docker/ubuntu_builder_12.04/docker-build.sh
+++ docker/ubuntu_builder_12.04/docker-build.sh
@@ -1,9 +1,12 @@
 #!/bin/sh
 
+# Determine the directory of the script
+SCRIPT_DIR="$(cd "$(dirname "$0")" && pwd)"
+
 # Release build
-docker build --platform linux/arm/v7 -t dilshodm/ubuntu-builder-arm:12.04 .
+docker build --platform linux/arm/v7 -t dilshodm/ubuntu-builder-arm:12.04 "$SCRIPT_DIR"
 
 # Development build
 docker build --platform linux/arm/v7 \
     --build-arg OPENSSL_CONFIG_ARGS="no-asm" \
-    -t dilshodm/ubuntu-builder-arm:12.04-devel .
+    -t dilshodm/ubuntu-builder-arm:12.04-devel "$SCRIPT_DIR"
