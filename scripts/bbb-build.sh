c55edf8355f1e8cb8c4e9c72d37abeb440c99b3f
Dilshod Mukhtarov <dilshodm@gmail.com>
2024-12-20 21:48:24 +0400

    Fix: bbb-build.sh didn't use devel option

--- scripts/bbb-build.sh
+++ scripts/bbb-build.sh
@@ -5,11 +5,6 @@
 # from https://medium.com/redbubble/running-a-docker-container-as-a-non-root-user-7d2e00f8ee15
 # and https://jtreminio.com/blog/running-docker-containers-as-current-host-user/
 
-#if [ "$#" -eq 1 ]; then
-#    echo "Usage: $0 [release|devel]"
-#    exit 1
-#fi
-
 if [ "$1" = "release" ]; then
     BUILD_DIR=build-bbb-release
     DOCKER_IMAGE=dilshodm/ubuntu-builder-arm:12.04
@@ -24,4 +19,4 @@ fi
 CMD="cmake -G Ninja -B '$BUILD_DIR' -D BBB_BUILD=ON . && cmake --build '$BUILD_DIR' --config Release && cd '$BUILD_DIR' && cpack -G DEB"
 echo $CMD
 
-docker container run --rm -it -v $(pwd):/src --workdir /src --user $(id -u):$(id -g) --platform linux/arm/v7 dilshodm/ubuntu-builder-arm:12.04 bash -c "$CMD"
+docker container run --rm -it -v $(pwd):/src --workdir /src --user $(id -u):$(id -g) --platform linux/arm/v7 $DOCKER_IMAGE bash -c "$CMD"
