3b2cb3778d1c445eb5f6765cf222211c81cad2d4
Dilshod Mukhtarov <dilshodm@gmail.com>
2024-12-31 13:24:54 +0400

    Send sdk version and game_code (instead of provider)

--- source/game_state_impl.cpp
+++ source/game_state_impl.cpp
@@ -20,8 +20,8 @@ GameStateImpl::GameStateImpl(std::unique_ptr<NetBase> net)
     m_net->authenticate();
 
     const auto &deviceInfo = m_net->deviceInfo();
-    // m_net->sendInstalled("sdk", SCORBIT_SDK_VERSION, true);
-    m_net->sendInstalled("provider", deviceInfo.gameCodeVersion, true);
+    m_net->sendInstalled("game_code", deviceInfo.gameCodeVersion, true);
+    m_net->sendInstalled("sdk", SCORBIT_SDK_VERSION, true);
 }
 
 void GameStateImpl::setGameStarted()
