bfc824d477621eef98d86c6047df8c65881aab23
Dilshod Mukhtarov <dilshodm@gmail.com>
2025-03-18 18:21:30 +0400

    Logger: set message limit to 511 chars

--- source/logger.cpp
+++ source/logger.cpp
@@ -9,7 +9,7 @@
 
 namespace {
 
-constexpr size_t MAX_LOG_MESSAGE_LENGTH = 512;
+constexpr size_t MAX_LOG_MESSAGE_LENGTH = 511;
 
 #if defined(__GNUC__) || defined(__clang__)
 #    define LIKELY(x) __builtin_expect(!!(x), 1)
