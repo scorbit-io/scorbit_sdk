609b6b5fd54dbb50fe1299d6c0aedfce467cdc82
Dilshod Mukhtarov <dilshodm@gmail.com>
2025-04-04 14:40:54 +0400

    Add archiver

---
+++ source/utils/archiver.h
@@ -0,0 +1,18 @@
+/****************************************************************************
+ *
+ * @author Dilshod Mukhtarov <dilshodm(at)gmail.com>
+ * Apr 2025
+ *
+ ****************************************************************************/
+
+#pragma once
+
+#include <string>
+
+namespace scorbit {
+namespace detail {
+
+bool extract(const std::string &archivePath, const std::string &outputDir);
+
+} // namespace detail
+} // namespace scorbit
