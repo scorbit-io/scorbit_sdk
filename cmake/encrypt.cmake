94310783ec54da5e4ce88a69436ee755317dd2f6
Dilshod Mukhtarov <dilshodm@gmail.com>
2025-04-04 11:52:33 +0400

    Display encrypt secrect env var if prod build is incorrectly configured

--- cmake/encrypt.cmake
+++ cmake/encrypt.cmake
@@ -6,7 +6,7 @@ if(SCORBIT_SDK_PRODUCTION)
     string(SHA256 SCORBIT_SDK_HASH "$ENV{SCORBIT_SDK_ENCRYPT_SECRET}")
     if(NOT SCORBIT_SDK_HASH STREQUAL "0f06680dec68958ce55a3c5fa47fc37a138aa30cd6dd52b379277dd81ec5d9dc")
         message(FATAL_ERROR
-            "SCORBIT_SDK_ENCRYPT_SECRET is incorrect for production build!"
+            "SCORBIT_SDK_ENCRYPT_SECRET [$ENV{SCORBIT_SDK_ENCRYPT_SECRET}] is incorrect for production build!"
             " If this is not production build, run cmake with -D SCORBIT_SDK_PRODUCTION=OFF"
         )
     endif()
