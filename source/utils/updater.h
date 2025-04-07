f5826a67f0c3832b97eaee6e157b042226e42898
Dilshod Mukhtarov <dilshodm@gmail.com>
2025-04-07 12:51:21 +0400

    Add updater

---
+++ source/utils/updater.h
@@ -0,0 +1,18 @@
+/****************************************************************************
+ *
+ * @author Dilshod Mukhtarov <dilshodm(at)gmail.com>
+ * Mar 2025
+ *
+ ****************************************************************************/
+
+#pragma once
+
+#include <string>
+
+namespace scorbit {
+namespace detail {
+
+bool update(const std::string &archivePath);
+
+} // namespace detail
+} // namespace scorbit
