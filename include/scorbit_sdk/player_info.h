8a3c10218c860c58f66d38af0e66df9493112dab
Dilshod Mukhtarov <dilshodm@gmail.com>
2025-05-31 15:03:35 +0400

    Add player id and picture url to C++ GameState

--- include/scorbit_sdk/player_info.h
+++ include/scorbit_sdk/player_info.h
@@ -16,9 +16,11 @@ namespace scorbit {
 using Picture = std::vector<uint8_t>; // The profile picture binary (jpg)
 
 struct PlayerInfo {
+    int64_t id;                       /// The player's ID
     std::string preferredName;        /// Either name or initials
     std::string name;                 /// The player's name to display
     std::string initials;             /// The player's initials, e.g. "DTM"
+    std::string pictureUrl;           /// The URL of the profile picture
     std::vector<uint8_t> picture;     /// The profile picture binary (jpg), can be empty
 };
 
