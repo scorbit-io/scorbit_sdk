efd4579e1a06f2e7cba869499fa71a575c16af2e
Dilshod Mukhtarov <dilshodm@gmail.com>
2025-10-15 20:12:17 +0400

    fix(python): in example forgot to set log's max length

--- examples/python_example/main.py
+++ examples/python_example/main.py
@@ -137,7 +137,7 @@ def main():
     print(f"Simple example of Scorbit SDK {scorbit.__version__} usage")
 
     # Setup logger
-    scorbit.add_logger_callback(logger_callback)
+    scorbit.add_logger_callback(logger_callback, 512) # 512 bytes max message length)
 
     # Create game state object
     gs = setup_game_state()
